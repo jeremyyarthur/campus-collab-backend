61149638ae2b8cbcd50c634e80b731ea0ac6ee84
Bradley <jeremyarthur9347@gmail.com>
2025-12-14 00:59:56 +0000

    Update database connection string for Postgres

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const { Pool } = require("pg")
 
 const pool = new Pool({
-    connectionString: "postgresql://jeremyyarthur:f0ucfaL0nXvy7XkNMg1xs2RZkB5xg0Pe@dpg-d4k169shg0os73advls0-a.oregon-postgres.render.com/campus_collab",
+    connectionString: "postgresql://arthur:qsCPWaXGxTxTvM5l4KYcWipeOb10vNo7@dpg-d4k169shg0os73advls0-a.oregon-postgres.render.com/campus_collab",
     ssl: {
         require: true,
         rejectUnauthorized: false,   
